a17464107b204afb40a49c1a57f2942bc248eff3
PaulPintang <pauljpintang@gmail.com>
2022-03-17 11:03:04 +0800

    install material ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Button } from "@material-ui/core";
 
 const Header = () => {
-  return <div>header</div>;
+  return (
+    <div>
+      <div className="flex justify-between items-center">
+        <h1 className="text-2xl font-semibold">Your Post</h1>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
